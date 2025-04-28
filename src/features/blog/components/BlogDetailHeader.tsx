916369943f5b226991809d51103b259ea67cddad
ivanfa434 <ivanfahrudinaziz123@gmail.com>
2025-04-28 15:28:51 +0700

    feat add delete-blog (#5)

--- src/features/blog/components/BlogDetailHeader.tsx
+++ src/features/blog/components/BlogDetailHeader.tsx
@@ -3,11 +3,21 @@ import { Blog } from "@/types/blog";
 import { FC } from "react";
 import { format } from "date-fns";
 import Image from "next/image";
+import { useAuthStore } from "@/stores/auth";
+import ModalDeleteBlog from "./ModalDeleteBlog";
+import useDeleteBlog from "@/hooks/api/blog/useDeleteBlog";
 
 interface BlogDetailHeaderProps {
   blog: Blog;
 }
 const BlogDetailHeader: FC<BlogDetailHeaderProps> = ({ blog }) => {
+  const { user } = useAuthStore();
+  const { mutateAsync: deleteBlog, isPending } = useDeleteBlog();
+
+  const handleDeleteBlog = async () => {
+    await deleteBlog(blog.id);
+  };
+
   return (
     <section className="mt-10 space-y-2">
       <Badge
@@ -18,10 +28,17 @@ const BlogDetailHeader: FC<BlogDetailHeaderProps> = ({ blog }) => {
       </Badge>
 
       <h1 className="text-3xl font-bold">{blog.title}</h1>
-      <p className="font-extralight">
-        {format(new Date(blog.createdAt), "dd MM yyy")} -{" "}
-        <span className="capitalize">{blog.user?.name}</span>
-      </p>
+
+      <div className="flex items-center justify-between">
+        <p className="font-extralight">
+          {format(new Date(blog.createdAt), "dd MM yyy")} -{" "}
+          <span className="capitalize">{blog.user?.name}</span>
+        </p>
+
+        {user?.id === blog.userId && (
+          <ModalDeleteBlog isPending={isPending} onClick={handleDeleteBlog} />
+        )}
+      </div>
 
       <div className="relative h-[400px]">
         <Image
